85d026cd61ead321b031048d2d9fbd0d8f31717c
Logalakshmi-A <logalakshmi91@gmail.com>
2025-02-17 19:34:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import Blocks from './components/Blocks'
+import Body from './components/Body'
+import Features from './components/Features'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Main from './components/Main'
+import Pricing from './components/Pricing'
+import Testimonial from './components/Testimonial'
+function App() {
+  
+  return (
+    <>
+      <div>
+        <h1>Recap Session</h1>
+        <Header />
+        <Main />
+        <Features />
+        <Body />
+        <Hero />
+        <Testimonial />
+        <Pricing />
+        <Blocks />
+        <Footer />
+        </div>
+    </>
+  )
+}
+
+export default App
